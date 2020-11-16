8c4008ac93c5182ec59603b11c2cb097838d2134
Richard Larancuente <richardllara05@gmail.com>
2020-11-15 21:56:55 -0500

    added layout.js to frontend and added middleware

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,21 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = express()
 const port = 5000
 
-app.use((req, res, next) => {
-    console.log(`Path: ${req.url} [${req.method}]`)
-    next()
-})
+// All the middleware being used
+const middleware = {
+    logger: (req, res, next) => {
+        console.log(`Path: ${req.url} [${req.method}]`)
+        next()
+    },
+    cors: cors()
+}
+
+app.use([middleware.cors, middleware.logger])
+
+
 
 app.get('/', (req, res) => {
     res.send('<h1>Hello</h1>')
